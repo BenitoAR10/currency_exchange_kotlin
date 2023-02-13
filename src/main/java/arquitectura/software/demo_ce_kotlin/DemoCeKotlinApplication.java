5dd58d2294806cfa45bad53915b2ec8681228fdd
unknown <https://github.com/BenitoAR10?tab=repositories>
2023-02-13 15:26:30 -0400

    logback

--- src/main/java/arquitectura/software/demo_ce_kotlin/DemoCeKotlinApplication.java
+++ src/main/java/arquitectura/software/demo_ce_kotlin/DemoCeKotlinApplication.java
@@ -1,5 +1,8 @@
 package arquitectura.software.demo_ce_kotlin;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoCeKotlinApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoCeKotlinApplication.class, args);
+
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+		JoranConfigurator configurator = new JoranConfigurator();
+		configurator.setContext(loggerContext);
+		loggerContext.reset();
+		try {
+			configurator.doConfigure("src/main/java/logback.xml");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
